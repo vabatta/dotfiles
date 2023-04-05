71014ff3a8c5e3129eb60ba6bf3b4835e38e2318
Valerio Battaglia <vabatta@gmail.com>
2023-04-05 17:59:21 +0200

    feat: add better system theme switch

--- config/nvim/lua/theme.lua
+++ config/nvim/lua/theme.lua
@@ -3,11 +3,26 @@ require("gruvbox").setup({
 	contrast = "soft"
 })
 
-local system_theme = vim.fn.system("defaults read -g AppleInterfaceStyle")
-if string.match(system_theme, '^Dark') == 'Dark' then
-	vim.o.background = "dark"
-else
-	vim.o.background = "light"
+-- Set theme based on system theme
+local function set_theme()
+	-- REVIEW: for some reason, this doesn't work all the time
+	-- local system_theme = vim.fn.system("zsh -i -c 'current_theme'")
+	local system_theme = vim.fn.system("zsh -c 'sh $DOTFILES/zsh/functions/current_theme'")
+	if system_theme == "dark" then
+		vim.o.background = "dark"
+	else
+		vim.o.background = "light"
+	end
 end
+
+-- Register for signal SIGUSR1 to change theme
+vim.api.nvim_create_autocmd("Signal", {
+	pattern = { "SIGUSR1" },
+	desc = "Change theme based on system theme",
+	callback = function(event) set_theme() end,
+})
+
+-- Call on load
+set_theme()
 
 vim.cmd([[colorscheme gruvbox]])

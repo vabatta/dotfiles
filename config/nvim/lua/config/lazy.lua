2698dbdddee7e896dd0596ea712674aba847f6b8
Valerio Battaglia <vabatta@gmail.com>
2023-12-28 20:05:45 +0100

    feat: add DAP extra plugin

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -23,6 +23,8 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
+		{ import = "lazyvim.plugins.extras.dap.nlua" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},

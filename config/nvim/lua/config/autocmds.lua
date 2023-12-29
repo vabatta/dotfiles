d4ad9cfb58a3ad9f8d8334e818d2d6ea9f4ee031
Valerio Battaglia <vabatta@gmail.com>
2023-12-29 02:25:00 +0100

    feat: add numertoggle cmd and extra plugin

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,25 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local group = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+	group = group,
+	pattern = "*",
+	callback = function()
+		if vim.wo.number and vim.fn.mode() ~= "i" then
+			vim.wo.relativenumber = true
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+	group = group,
+	pattern = "*",
+	callback = function()
+		if vim.wo.number then
+			vim.wo.relativenumber = false
+		end
+	end,
+})
